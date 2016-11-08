8db0f3d5d5b96040f8775177ab42bc038d1d8024
geforcexfx <geforcexfx@yahoo.com>
2016-11-08 11:27:49 +0100

    comment post

---
+++ src/WebApi/JsonModels/CommentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.JsonModels
+{
+    public class CommentViewModel
+    {
+
+        public string Url { get; set; }
+        public int PostId { get; set; }
+        public int userID { get; set; }
+        public string commentBody { get; set; }
+        public string commentCreationDate { get; set; }
+    }
+}
